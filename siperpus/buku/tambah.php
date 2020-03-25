136488c3eb083ea75c97b5f18a37e0aef660a117
Memoreza Sabana <53498578+memorezasabana@users.noreply.github.com>
2020-03-25 14:31:56 +0700

    Update tambah.php

--- siperpus/buku/tambah.php
+++ siperpus/buku/tambah.php
@@ -6,25 +6,25 @@ include '../aset/header.php';
         <div class="col-md-9">
             <div class="card">
                 <div class="card-header">
-                <h2>Tambah Data Buku</h2>
+                    <h2>Tambah Anggota</h2>
                 </div>
                 <div class="card-body">
-                <form method="post" action="proses-tambah.php">
+                    <form method="post" action="proses-tambah.php">
                         <div class="form-group">
                             <label for="judul">Judul</label>
-                            <input type="text" class="form-control" name="judul" id="judul" placeholder="Masukkan Judul Buku">
+                            <input type="text" class="form-control" name="judul" id="judul" placeholder="Masukkan Judul buku">
                         </div>
                         <div class="form-group">
                             <label for="penerbit">Penerbit</label>
-                            <input type="text" class="form-control" name="penerbit" id="penerbit" placeholder="Masukkan Penerbit">
+                            <input type="text" class="form-control" name="penerbit" id="penerbit" placeholder="Masukkan Nama penerbit">
                         </div>
                         <div class="form-group">
                             <label for="pengarang">Pengarang</label>
-                            <input type="text" class="form-control" name="pengarang" id="pengarang" placeholder="Masukkan Pengarang">
-                            </div>
+                            <input type="text" class="form-control" name="pengarang" id="pengarang" placeholder="Masukkan Nama pengarang">
+                        </div>
                         <div class="form-group">
                             <label for="ringkasan">Ringkasan</label>
-                            <input type="text" class="form-control" name="ringkasan" id="ringkasan" placeholder="Masukkan Ringkasan">
+                            <textarea class="form-control" name="ringkasan" id="ringkaasan" placeholder="Masukkan Ringkasan Cerita"></textarea>
                         </div>
                         <div class="form-group">
                             <label for="cover">Cover</label>
@@ -32,17 +32,16 @@ include '../aset/header.php';
                         </div>
                         <div class="form-group">
                             <label for="stok">Stok</label>
-                            <input type="text" class="form-control" name="stok" id="stok" placeholder="Masukkan Stok">
+                            <input type="number" class="form-control" name="stok" id="stok" placeholder="Masukkan jumlah stok">
                         </div>
                         <div class="form-group">
-                            <label for="kategori">Kategori</label><br>
-                            <select nama="id_kategori" id="sel">
-                            <option value="">Pilih Kategori</option>
-                            <option value="Fiksi">Fiksi</option>
-                            <option value="Non Fiksi">Non Fiksi</option>
+                            <label for="id_Kategori">Id Kategori</label><br>
+                            <select name="id_kategori" id="sel">
+                                <option value="">Pilih Kategori</option>
+                                <option value="Fiksi">Fiksi</option>
+                                <option value="Non">Non Fiksi</option>
                             </select>
                         </div>
-
                         <button type="submit" class="btn btn-primary" name="simpan">Simpan</button>
                     </form>
                 </div>
@@ -50,7 +49,6 @@ include '../aset/header.php';
         </div>
     </div>
 </div>
-
 <?php
 include '../aset/footer.php';
 ?>
